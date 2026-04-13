d686e48517f26aa891a2a390e8bcad8da7546910
reinicheum <gyrlin.ediger@yahoo.com>
2026-04-13 16:26:25 +0300

    v.1.0

--- Core/Src/platform/lab4_adc/lab4_adc.h
+++ Core/Src/platform/lab4_adc/lab4_adc.h
@@ -7,5 +7,7 @@ void plt_adc_start(void);
 void plt_adc_stop(void);
 int plt_adc_conversion_poll(void);
 uint32_t plt_adc_get_value(void);
+float plt_adc_get_voltage(void);
+float plt_adc_get_temperature(void);
 
 #endif /* SRC_PLATFORM_LAB4_ADC_LAB4_ADC_H_ */
--- Core/Src/platform/lab5_usart/lab5_usart.h
+++ Core/Src/platform/lab5_usart/lab5_usart.h
@@ -1,6 +1,10 @@
 #ifndef SRC_PLATFORM_LAB5_USART_LAB5_USART_H_
 #define SRC_PLATFORM_LAB5_USART_LAB5_USART_H_
 
+#include <stdint.h>
 
+void plt_uart_init(void);
+void plt_uart_send(const char* data);
+void plt_uart_print(const char* format, ...);
 
-#endif /* SRC_PLATFORM_LAB5_USART_LAB5_USART_H_ */
+#endif
