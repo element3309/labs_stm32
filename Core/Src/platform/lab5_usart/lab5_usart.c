6c987b32cef78a023bac6b4093fabe907520578c
Bamovskiy <frundinv@gmail.com>
2026-05-04 16:32:21 +0300

    some changes for usart lab

--- Core/Src/platform/lab5_usart/lab5_usart.c
+++ Core/Src/platform/lab5_usart/lab5_usart.c
@@ -9,6 +9,7 @@ extern UART_HandleTypeDef huart1;
 static char tx_buf[256];
 static char rx_byte = 0;
 
+volatile int uart_rx_flag = 0;
 /* init UART и стартового меню */
 void plt_uart_init(void)
 {
@@ -37,30 +38,45 @@ void plt_uart_print(const char *format, ...)
     plt_uart_send(tx_buf);
 }
 
+int plt_uart_is_available(void)
+{
+	return uart_rx_flag;
+}
+
+void plt_uart_proccess(void)
+{
+	if(plt_uart_is_available())
+	{
+		uart_rx_flag = 0;
+
+	    plt_uart_send(&rx_byte); /* отправка нажатого символа обратно */
+
+	    if (rx_byte == '1') /* если 1, то вкл */
+	    {
+	        HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_SET);
+	        plt_uart_send("\r\n[OK] LED ON\r\nWaiting input: ");
+	    }
+	    else if (rx_byte == '0') /* если 0, то выкл */
+	    {
+	        HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_RESET);
+	        plt_uart_send("\r\n[OK] LED OFF\r\nWaiting input: ");
+	    }
+	    else
+	    {
+	        if (rx_byte != '\r' && rx_byte != '\n')
+	        {
+	            plt_uart_send("\r\n[ERROR] Use 1 or 0 only\r\nWaiting input: ");
+	        }
+	    }
+
+	    HAL_UART_Receive_IT(&huart1, (uint8_t*)&rx_byte, 1);
+	}
+}
+
 /* колбэк при получении байта */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
     if (huart->Instance != USART1) return;
 
-    plt_uart_send(&rx_byte); /* отправка нажатого символа обратно */
-
-    if (rx_byte == '1') /* если 1, то вкл */
-    {
-        HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_SET);
-        plt_uart_send("\r\n[OK] LED ON\r\nWaiting input: ");
-    }
-    else if (rx_byte == '0') /* если 0, то выкл */
-    {
-        HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_RESET);
-        plt_uart_send("\r\n[OK] LED OFF\r\nWaiting input: ");
-    }
-    else
-    {
-        if (rx_byte != '\r' && rx_byte != '\n')
-        {
-            plt_uart_send("\r\n[ERROR] Use 1 or 0 only\r\nWaiting input: ");
-        }
-    }
-
-    HAL_UART_Receive_IT(&huart1, (uint8_t*)&rx_byte, 1);
+    uart_rx_flag = 1;
 }
--- Core/Src/platform/lab5_usart/lab5_usart.h
+++ Core/Src/platform/lab5_usart/lab5_usart.h
@@ -4,5 +4,7 @@
 void plt_uart_init(void);
 void plt_uart_send(const char *data);
 void plt_uart_print(const char *format, ...);
+int plt_uart_is_available(void);
+void plt_uart_proccess(void);
 
 #endif
