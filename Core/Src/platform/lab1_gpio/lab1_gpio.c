7f708ca3d3f69179d011ddcd61477595d33940b0
Rrrrandanna <kristina.kochetkova6853@gmail.com>
2026-05-11 23:38:04 +0300

    lab5 completed

--- Core/Src/platform/lab1_gpio/lab1_gpio.c
+++ Core/Src/platform/lab1_gpio/lab1_gpio.c
@@ -9,8 +9,8 @@
 
 /* Директива препроцессора - макроопределение
  * создаёт макрос (слева), который препроцессор заменяет на указанный справа текст */
-#define LED_GPIO_Port	LED_BLUE_GPIO_Port
-#define LED_Pin 		LED_BLUE_Pin
+#define LED_GPIO_Port   USER_LED_GPIO_Port
+#define LED_Pin         USER_LED_Pin
 
 void plt_gpio_toggle(void)
 {
