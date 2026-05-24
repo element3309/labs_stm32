e16cb127ddcb767837f2908930055bc1b689b9eb
Alla <murzinaalla764@gmail.com>
2026-05-25 00:29:04 +0300

    Update platform.c

--- Core/Src/platform/platform.c
+++ Core/Src/platform/platform.c
@@ -1,28 +1,51 @@
 /* Директива препроцессора для подключения заголовочного файла */
 #include "platform.h"
+
 #include "main.h"
-//#include "lab1_gpio/lab1_gpio.h"
-//#include "lab2_timer/lab2_timer.h"
-//#include "lab3_stepper/lab3_stepper.h"
+#include "lab1_gpio/lab1_gpio.h"
+#include "lab2_timer/lab2_timer.h"
+#include "lab3_stepper/lab3_stepper.h"
 #include "lab4_adc/lab4_adc.h"
+#include "platform_def.h"  // правильный путь
+
 extern UART_HandleTypeDef huart1;
+
 /* Однократный вызов */
 int plt_init(void)
 {
-
-	return 0;
+    return 0;
 }
 
+int direction = 1;
+int step_counter = 0;
+int steps_per_rotation = 8;
 
 /* Повторяющийся вызов */
 void plt_process(void)
 {
-	/* Устанавливаем задержку */
-	plt_delay(500);
-}
+    uint32_t adc_value;
+    float voltage;
 
-/* Перенести в main.c в user code */
-//void plt_delay(uint32_t delay_ms)
-//{
-//	HAL_Delay(delay_ms);
-//}
+    // Запуск АЦП
+    plt_adc_start();
+
+    if (plt_adc_conversion_poll() == PLT_OK)
+    {
+        adc_value = plt_adc_get_value();
+        voltage = plt_adc_get_voltage();
+
+        if (adc_value > 3000)
+        {
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_6);
+        }
+        else
+        {
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+        }
+    }
+    // Останов АЦП
+    plt_adc_stop();
+
+    // Задержка
+    HAL_Delay(100);
+}
