c00e8b96f58b7ce9a045bd126420919f09f2112b
Alla <murzinaalla764@gmail.com>
2026-05-25 00:32:16 +0300

    Update platform.c

--- Core/Src/platform/platform.c
+++ Core/Src/platform/platform.c
@@ -1,28 +1,28 @@
 /* Директива препроцессора для подключения заголовочного файла */
 #include "platform.h"
+
 #include "main.h"
-//#include "lab1_gpio/lab1_gpio.h"
-//#include "lab2_timer/lab2_timer.h"
-//#include "lab3_stepper/lab3_stepper.h"
+#include "lab1_gpio/lab1_gpio.h"
+#include "lab2_timer/lab2_timer.h"
+#include "lab3_stepper/lab3_stepper.h"
 #include "lab4_adc/lab4_adc.h"
+#include "platform_def.h"  // правильный путь
+
 extern UART_HandleTypeDef huart1;
+
+
+void plt_process(void){
+	HAL_Delay(500);
+}
+
 /* Однократный вызов */
 int plt_init(void)
 {
-
-	return 0;
+	plt_timer_set(1999);
+	plt_timer_start_irq();
+    return 0;
 }
 
-
-/* Повторяющийся вызов */
-void plt_process(void)
-{
-	/* Устанавливаем задержку */
-	plt_delay(500);
+void plt_timer_irq_cb(void){
+	HAL_GPIO_TogglePin(USER_LED_GPIO_Port, USER_LED_Pin);
 }
-
-/* Перенести в main.c в user code */
-//void plt_delay(uint32_t delay_ms)
-//{
-//	HAL_Delay(delay_ms);
-//}
