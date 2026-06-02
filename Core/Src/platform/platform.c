d111c49aa43568f9ec2f097f5bbd332edd93e491
hioren7 <daryalapaeva7@gmail.com>
2026-06-02 22:02:51 +0300

    lab3 done

--- Core/Src/platform/platform.c
+++ Core/Src/platform/platform.c
@@ -3,8 +3,8 @@
 #include "main.h"
 //#include "lab1_gpio/lab1_gpio.h"
 //#include "lab2_timer/lab2_timer.h"
-//#include "lab3_stepper/lab3_stepper.h"
-#include "lab4_adc/lab4_adc.h"
+#include "lab3_stepper/lab3_stepper.h"
+//#include "lab4_adc/lab4_adc.h"
 extern UART_HandleTypeDef huart1;
 /* Однократный вызов */
 int plt_init(void)
@@ -17,8 +17,9 @@ int plt_init(void)
 /* Повторяющийся вызов */
 void plt_process(void)
 {
+	plt_stepper(-1)
 	/* Устанавливаем задержку */
-	plt_delay(500);
+	plt_delay(100);
 }
 
 /* Перенести в main.c в user code */
